67af59c25093404ad9905512bbbd2bb3d7f42aa7
Godefroy <courtin.godefroy@gmail.com>
2020-10-21 19:37:24 +0200

    initialisation chat-app

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = ({ users }) => (
+    < div className="Contact" >
+        <img className="avatar" src="https://randomuser.me/api/portraits/men/68.jpg" alt="" />
+        <div>
+            <h2 className="name">{users.name}</h2>
+            <div className="status">
+                <div className={users.online ? "status-online" : "status-offline"}></div>
+                <p className="status-text">{users.online ? "Online" : "Offline"}</p>
+            </div>
+        </div>
+    </div >
+)
+
+export default Contact;
